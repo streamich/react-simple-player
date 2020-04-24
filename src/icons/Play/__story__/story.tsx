a181bf056b07dd8e476007c12c7309406cb0be0d
streamich <streamich@gmail.com>
2020-04-24 17:19:46 +0200

    feat: 🎸 add <Play> icon

---
+++ src/icons/Play/__story__/story.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import {storiesOf} from '@storybook/react';
+import Play from '..';
+
+storiesOf('icons/Play', module)
+  .add('Default', () => {
+    return <Play />
+  });
