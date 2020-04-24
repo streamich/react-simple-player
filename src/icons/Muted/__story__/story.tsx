1297a783946c9f0810f747a7173c305b659598a0
streamich <streamich@gmail.com>
2020-04-24 18:45:56 +0200

    feat: 🎸 add <Muted> and <Volume> icons

---
+++ src/icons/Muted/__story__/story.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import {storiesOf} from '@storybook/react';
+import Muted from '..';
+
+storiesOf('icons/Muted', module).add('Default', () => {
+  return <Muted />;
+});
